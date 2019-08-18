9fd531ca73899e47683dd18c8a5b81f9026370f1
Stef <git@srilq.email>
2019-08-18 15:40:57 +0100

    Add home link

--- components/Nav.js
+++ components/Nav.js
@@ -10,7 +10,9 @@ const IconLink = ({ name, href, children }) => (
 
 const Nav = () => (
   <header className="flex flex-column flex-row-ns justify-between items-center">
-    <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
+    <a href="/" className="no-underline">
+      <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
+    </a>
     <ul className="list pa0 ma0 f5 ttl flex flex-row">
       <li><IconLink href="https://www.instagram.com/e.seilide/" name="INSTAGRAM" /></li>
       <li><IconLink href="https://e-seilide.tumblr.com" name="TUMBLR" /></li>
