78163bc3edad4f9724e0b7591465f8c575e50aa1
Stef <git@srilq.email>
2019-09-08 19:59:08 +0100

    Use @renditions/react-img

--- components/Image.js
+++ components/Image.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import getSizes from '@renditions/get-sizes';
-import getSrcset, { sortRenditions } from '@renditions/get-srcset';
+import Img from '@renditions/react-img';
 import { getImageSrc } from '../utils/images';
 
-const Image = ({ image, renditions, width = '100vw', ...rest }) => {
-  const sortedRenditions = sortRenditions([...renditions]);
-
-  const renditionsWithSrc = sortedRenditions.map(rendition => ({
-    ...rendition,
-    src: getImageSrc(image, rendition)
-  }));
-
-  const imageProps = {
-    src: getImageSrc(image, sortedRenditions[0]),
-    srcSet: getSrcset(renditionsWithSrc),
-    sizes: getSizes({ width })
-  };
-
-  return (
-    <img
-      alt={image.altText}
-      {...imageProps}
-      {...rest}
-    />
-  );
-};
+const Image = ({ image, renditions, width = '100vw', ...rest }) => (
+  <Img
+    alt={image.altText}
+    getSrc={getImageSrc.bind(null, image)}
+    renditions={renditions}
+    width={width}
+    {...rest}
+  />
+);
 
 export default Image;
