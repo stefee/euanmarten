5a3c71d14458bd7007b6911ab37ece9d3d297ba5
Stef <stef.jones@condenast.com>
2020-07-11 13:13:13 +0100

    fix: use function component for App

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,23 @@
 import { Fragment } from 'react';
-import App from 'next/app';
 import Head from 'next/head';
 import 'tachyons';
 import './styles.css';
 import Nav from '../components/Nav';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+const App = ({ Component, pageProps }) => (
+  <Fragment>
+    <Head>
+      <title>Euan Marten Portfolio</title>
+      <meta name="description" content="Euan Marten is an illustrator & designer with a BA in Illustration from Norwich University of the Arts." />
+    </Head>
 
-    return (
-      <Fragment>
-        <Head>
-          <title>Euan Marten Portfolio</title>
-          <meta name="description" content="Euan Marten is an illustrator & designer with a BA in Illustration from Norwich University of the Arts." />
-        </Head>
+    <div className="sans-serif">
+      <div className="pa4">
+        <Nav />
+      </div>
+      <Component {...pageProps} />
+    </div>
+  </Fragment>
+);
 
-        <div className="sans-serif">
-          <div className="pa4">
-            <Nav />
-          </div>
-          <Component {...pageProps} />
-        </div>
-      </Fragment>
-    );
-  }
-}
-
-export default MyApp;
+export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -21,4 +21,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default MyDocument;
