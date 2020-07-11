d852cda6fba36b018dc188a57fb4267b146f7bb8
Stef <stef.jones@condenast.com>
2020-07-11 13:06:45 +0100

    fix: rename style.css to styles.css

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import App from 'next/app';
 import Head from 'next/head';
 import 'tachyons';
-import './style.css';
+import './styles.css';
 import Nav from '../components/Nav';
 
 class MyApp extends App {
