39a97517f6377d6e3024cee2659e0c8d6b6f39f9
Stef <stef.jones@condenast.com>
2020-07-11 18:49:17 +0100

    fix: use next/link for home link

--- components/Nav.js
+++ components/Nav.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Icon from './Icon';
 
 const IconLink = ({ name, href, children, label }) => (
@@ -9,9 +10,11 @@ const IconLink = ({ name, href, children, label }) => (
 
 const Nav = () => (
   <header className="flex flex-column flex-row-ns justify-between items-center">
-    <a href="/" className="no-underline" aria-label="Home">
-      <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
-    </a>
+    <Link href="/">
+      <a className="no-underline" aria-label="Home">
+        <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
+      </a>
+    </Link>
     <ul className="list pa0 ma0 f5 ttl flex flex-row">
       <li><IconLink href="https://www.instagram.com/e.seilide/" name="INSTAGRAM" /></li>
       <li><IconLink href="https://e-seilide.tumblr.com" name="TUMBLR" /></li>
