71389c35fdc5463e10afac43d60a1a4af414782f
Stef <stef.jones@condenast.com>
2020-07-12 17:31:47 +0100

    fix: tweak nav DOM

--- components/Nav.js
+++ components/Nav.js
@@ -10,11 +10,13 @@ const IconLink = ({ name, href, children, label }) => (
 
 const Nav = () => (
   <header className="flex flex-column flex-row-ns justify-between items-center">
-    <Link href="/">
-      <a className="no-underline" aria-label="Home">
-        <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
-      </a>
-    </Link>
+    <div>
+      <Link href="/">
+        <a className="no-underline" aria-label="Home">
+          <h1 className="tc tl-ns ma0 mb4 mb0-ns mr4-ns f2 fw2 normal ttl">Euan Marten</h1>
+        </a>
+      </Link>
+    </div>
     <ul className="list pa0 ma0 mr2 mr0-ns f5 ttl flex flex-row">
       <li><IconLink href="https://www.instagram.com/e.seilide/" name="INSTAGRAM" /></li>
       <li><IconLink href="https://e-seilide.tumblr.com" name="TUMBLR" /></li>
