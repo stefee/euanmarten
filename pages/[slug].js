1a56f6c208c176be979efc554318d6afd18210f6
Stef <stef.jones@condenast.com>
2020-07-18 17:19:17 +0100

    feat: add project links on portfolio page

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { promises as fs } from 'fs';
+import Link from 'next/link';
 import { splitArrayAlternating } from '../utils/arrays';
 import Image from '../components/Image';
 import ColumnLayout from '../components/ColumnLayout';
@@ -8,7 +9,7 @@ import Lightbox from '../components/Lightbox';
 const THUMBNAIL_COLUMNS = 2;
 const THUMBNAIL_PADDING = 1;
 
-const Thumbnail = ({ image, renditions, onClick }) => (
+const ThumbnailButton = ({ image, renditions, onClick }) => (
   <div className="Thumbnail mb2">
     <button
       title="View Image"
@@ -21,24 +22,92 @@ const Thumbnail = ({ image, renditions, onClick }) => (
   </div>
 );
 
-const ThumbnailColumn = ({ items, images, renditions, setLightboxImage }) => (
-  <div>
-    {items.map(item => {
-      const image = images.find(data => data.filename === item.filename);
+const ThumbnailLink = ({ image, renditions, href, as }) => (
+  <div className="Thumbnail mb2">
+    <Link
+      href={href}
+      as={as}
+    >
+      <a className="pa0 db w-100">
+        <Image image={image} renditions={renditions} width="50vw" className="w-100 db" />
+      </a>
+    </Link>
+  </div>
+);
 
-      return (
-        <Thumbnail
-          key={image.filename}
-          image={image}
-          renditions={renditions}
-          onClick={() => setLightboxImage(image)}
-        />
-      );
+const ThumbnailColumn = ({ items, images, projects, renditions, setLightboxImage }) => (
+  <div>
+    {items.map((item, index) => {
+      switch(item.type) {
+        case 'image':
+          if (!item.filename) {
+            console.error(`Invalid data: item ${index} in column has type 'image' but does not have a filename property`);
+            return null;
+          }
+
+          const image = images.find(data => data.filename === item.filename);
+
+          if (!image) {
+            console.error(`Missing image data for ${item.filename}`);
+            return null;
+          }
+
+          return (
+            <ThumbnailButton
+              key={image.filename}
+              image={image}
+              renditions={renditions}
+              onClick={() => setLightboxImage(image)}
+            />
+          );
+        case 'project':
+          if (!item.slug) {
+            console.error(`Invalid data: item ${index} in column has type 'project' but does not have a slug property`);
+            return null;
+          }
+
+          const project = projects.find(data => data.slug === item.slug);
+
+          if (!project) {
+            console.error(`Missing project data for ${item.slug}`);
+            return null;
+          }
+
+          if (!project.thumbnailImage) {
+            console.error(`Invalid data: project ${project.slug} does not have a thumbnailImage property`)
+            return null;
+          }
+
+          if (!project.thumbnailImage.filename) {
+            console.error(`Invalid data: project ${project.slug} thumbnailImage does not have a filename property`)
+            return null;
+          }
+
+          const thumbnailImage = images.find(data => data.filename === project.thumbnailImage.filename);
+
+          if (!thumbnailImage) {
+            console.error(`Missing image data for ${project.thumbnailImage.filename}`);
+            return null;
+          }
+
+          return (
+            <ThumbnailLink
+              key={project.slug}
+              image={thumbnailImage}
+              renditions={renditions}
+              href="/project/[slug]"
+              as={`/project/${project.slug}`}
+            />
+          );
+        default:
+          console.error(`Invalid images data: item ${index} in column did not have a recognised type`);
+          return null;
+      }
     })}
   </div>
 );
 
-const Portfolio = ({ data: { items, images, renditions } }) => {
+const Portfolio = ({ data: { items, images, projects, renditions } }) => {
   const [lightboxImage, setLightboxImage] = useState(null);
 
   const isLightboxOpen = !!lightboxImage;
@@ -57,6 +126,7 @@ const Portfolio = ({ data: { items, images, renditions } }) => {
             key={i}
             items={items}
             images={images}
+            projects={projects}
             renditions={renditions}
             setLightboxImage={setLightboxImage}
           />
@@ -76,7 +146,7 @@ export const getStaticProps = async ({ params }) => {
   const imagesData = await fs.readFile('./images.json', { encoding: 'utf-8' });
   const parsedImagesData = JSON.parse(imagesData);
 
-  const { portfolios, images, renditions } = parsedImagesData;
+  const { portfolios, images, projects, renditions } = parsedImagesData;
 
   const portfolioData = portfolios.find(portfolio => portfolio.slug === params.slug);
 
@@ -87,6 +157,7 @@ export const getStaticProps = async ({ params }) => {
       data: {
         items,
         images,
+        projects,
         renditions,
       }
     }
