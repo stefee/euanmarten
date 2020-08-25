a38211659424830aa9b25f142ffb6abda5f2e0e1
Stef <stef.jones@condenast.com>
2020-08-25 12:43:12 +0100

    fix: project thumbnail heading padding-bottom

--- pages/[slug].js
+++ pages/[slug].js
@@ -120,7 +120,7 @@ const ThumbnailColumn = ({ items, images, projects, appConfig, setLightboxImage 
                 imageProps={thumbnailImageProps}
               />
               <div className="flex flex-column flex-row-ns items-center justify-center pa2 pb3 pt0 bb b--moon-gray lh-title fw2 f4 f3-m f2-l">
-                <h2 className="mb2 tc fw2 mt0 mb0 f4 f3-m f2-l">{project.title}</h2>
+                <h2 className="tc fw2 mt0 mb2 mb0-ns f4 f3-m f2-l">{project.title}</h2>
                 <Link href="/project/[slug]" as={`/project/${project.slug}`}>
                   <a className="ml3-ns f5 flex items-center">View Project</a>
                 </Link>
