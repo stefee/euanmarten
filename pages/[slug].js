fc524cba9e2c1a34c9ddefd3f90ef7e03bb7b6df
Stef <stef.jones@condenast.com>
2020-08-25 15:13:24 +0100

    fix: nav link styles

--- pages/[slug].js
+++ pages/[slug].js
@@ -145,11 +145,13 @@ const Portfolio = ({ appConfig, data: { slug, items, images, projects } }) => {
       <nav className="ph4 pt2 w-100 w-20-l">
         <div className="sticky-l top-2">
           <ul className="flex flex-row flex-column-l justify-center justify-start-l list pa0 ma0 mr2 mr0-ns f4 fw2 ttl">
-            <li className="mb3 mr4 mr0-l">
-              <Link href="/[slug]" as="/illustration"><a className="no-underline underline-hover">illustration</a></Link>
+            <li className="mb3 mr4 mr0-l flex flex-row items-center">
+              <span className={`f6 mr2${slug === 'illustration' ? '' : ' o-0'}`}>•</span>
+              <Link href="/[slug]" as="/illustration"><a className="no-underline">illustration</a></Link>
             </li>
-            <li className="mb3">
-              <Link href="/[slug]" as="/design"><a className="no-underline underline-hover">design</a></Link>
+            <li className="mb3 flex flew-row items-center">
+              <span className={`f6 mr2${slug === 'design' ? '' : '  o-0'}`}>•</span>
+              <Link href="/[slug]" as="/design"><a className="no-underline">design</a></Link>
             </li>
           </ul>
         </div>
