b9823ac7a243ee9762afd44d7c83f5753517a52e
Stef <stef.jones@condenast.com>
2020-08-25 12:54:40 +0100

    fix: tweak link styles

--- pages/[slug].js
+++ pages/[slug].js
@@ -119,10 +119,10 @@ const ThumbnailColumn = ({ items, images, projects, appConfig, setLightboxImage 
                 as={`/project/${project.slug}`}
                 imageProps={thumbnailImageProps}
               />
-              <div className="flex flex-column flex-row-ns items-center justify-center pa2 pb3 bb b--moon-gray lh-title fw2 f4 f3-m f2-l">
-                <h2 className="tc fw2 mt0 mb2 mb0-ns f4 f3-m f2-l">{project.title}</h2>
+              <div className="flex flex-column flex-row-l items-center justify-center pa2 pb3 bb b--moon-gray lh-title f4 f3-m f2-l">
+                <h2 className="tc fw2 mt0 mb2 mb0-l f4 f3-m f2-l">{project.title}</h2>
                 <Link href="/project/[slug]" as={`/project/${project.slug}`}>
-                  <a className="ml3-ns f5 flex items-center">View Project</a>
+                  <a className="tc ml3-l f5 flex items-center">View Project</a>
                 </Link>
               </div>
             </div>
